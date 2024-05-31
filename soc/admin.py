2e2c3eb2140da60f41f3b8af8e7ecec7188cfec8
Marek Goliaš <golias.marek.06@gmail.com>
2024-05-31 11:40:16 +0200

    modely + naplnena db

---
+++ soc/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models here.
+admin.site.register(Tema)
+admin.site.register(Student)
+admin.site.register(Ucitel)
+admin.site.register(Odbor)
+admin.site.register(Dostupnost)
---
+++ soc/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+# dorobit class Meta
+
+class Ucitel(models.Model):
+    meno = models.CharField(max_length=30)
+    priezvisko = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    heslo = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.meno} {self.meno}, {self.email}, {self.heslo}'
+
+
+class Student(models.Model):
+    meno = models.CharField(max_length=30)
+    priezvisko = models.CharField(max_length=30)
+    email = models.CharField(max_length=100)
+    heslo = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return f'{self.meno} {self.meno}, {self.email}, {self.heslo}'
+
+class Odbor(models.Model):
+    nazov = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.nazov}'
+
+
+class Dostupnost(models.Model):
+    nazov = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f'{self.nazov}'
+
+class Tema(models.Model):
+    nazov = models.CharField(max_length=100)
+    popis = models.TextField()
+    konzultant = models.ForeignKey(Ucitel, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=True)
+    odbor = models.ForeignKey(Odbor, on_delete=models.CASCADE)
+    dostupnost = models.ForeignKey(Dostupnost, on_delete=models.CASCADE)
+    pocet_kontrol = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.nazov}, {self.popis}, {self.konzultant}, {self.student}, {self.odbor}, {self.dostupnost}, {self.pocet_kontrol}'
