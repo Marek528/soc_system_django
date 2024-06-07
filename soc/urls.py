30f31bcb8760a42c23030e9fa4eb08602b6097ec
Marek Goliaš <golias.marek.06@gmail.com>
2024-06-07 11:29:59 +0200

    samostatne temy + base form

--- soc/urls.py
+++ soc/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('temy/', views.vypis_tem, name='vypis-tem'),
     path('ucitel/<id>/', views.vypis_ucitela, name='vypis-ucitela'),
     path('student/<id>/', views.vypis_studenta, name='vypis-studenta'),
+    path('temy_samostatne/', views.vypis_tem_samostatne, name='vypis-tem-samostatne'),
     path('tema/<id>/', views.vypis_temy, name='vypis-temy'),
     path('statistiky/', views.statistiky, name='statistiky'),
+    path('pridanie_temy/', views.pridat_temu, name='pridanie-temy'),
 ]
--- soc/views.py
+++ soc/views.py
@@ -15,6 +15,10 @@ def vypis_studenta(request, id):
     temy = Tema.objects.filter(student=student)
     return render(request, 'soc/student.html', {'temy': temy, 'student': student})
 
+def vypis_tem_samostatne(request):
+    temy = Tema.objects.all().order_by('dostupnost', 'id')
+    return render(request, 'soc/temy_samostatne.html', {'temy': temy})
+
 def vypis_temy(request, id):
     tema = Tema.objects.get(id=id)
     return render(request, 'soc/tema.html', {'tema': tema})
@@ -29,3 +33,18 @@ def statistiky(request):
     pocet_cakajucich = Tema.objects.filter(dostupnost__nazov='čakajúce na schválenie').count()
     return render(request, 'soc/statistiky.html', {'pocet_ucitelov': pocet_ucitelov, 'pocet_studentov': pocet_studentov, 'pocet_tem': pocet_tem,
                                                   'pocet_volnych': pocet_volnych, 'pocet_obsadenych': pocet_obsadenych, 'pocet_cakajucich': pocet_cakajucich})
+
+def pridat_temu(request):
+    studenti = Student.objects.all()
+    ucitelia = Ucitel.objects.all()
+    odbory = Odbor.objects.all()
+    dostupnosti = Dostupnost.objects.all()
+
+    # kontroly, ktore treba osetrit:
+    # 1. ci je konzultant + odbor + dostupnost vybrane
+    # 2. ak je student zvoleny ako -, v tom pripade skontrolovat ci je dostupnost zadana ako volne a pocet kontrol je 0
+    # 3. ci uz ziak nema ine prace/temy (da sa to osetrit rovno aj tu -> bude to asi lepsie)
+    if (request.method == "GET"):
+        return render(request, 'soc/add_tema.html', {"studenti":studenti, "ucitelia":ucitelia, "odbory":odbory, "dostupnosti":dostupnosti})
+    else:
+        return
