5478378e88e683bf854e6410e8ac728b8f116e4b
Marek528 <golias.marek.06@gmail.com>
2024-06-08 19:03:32 +0200

    pridavanie tem funguje (osetrene aj chyby pri vyplnani formu)

--- soc/views.py
+++ soc/views.py
@@ -43,8 +43,45 @@ def pridat_temu(request):
     # kontroly, ktore treba osetrit:
     # 1. ci je konzultant + odbor + dostupnost vybrane
     # 2. ak je student zvoleny ako -, v tom pripade skontrolovat ci je dostupnost zadana ako volne a pocet kontrol je 0
-    # 3. ci uz ziak nema ine prace/temy (da sa to osetrit rovno aj tu -> bude to asi lepsie)
+    # 3. ci uz ziak nema ine prace/temy
     if (request.method == "GET"):
         return render(request, 'soc/add_tema.html', {"studenti":studenti, "ucitelia":ucitelia, "odbory":odbory, "dostupnosti":dostupnosti})
     else:
-        return
+        konzultant = request.POST['konzultant']
+        odbor = request.POST['odbor']
+        dostupnost = request.POST['dostupnost']
+        student = request.POST['student']
+
+        if konzultant == "----------------------" or odbor == "----------------------" or dostupnost == "----------------------" or student == "----------------------":
+            return render(request, 'soc/add_tema.html', {"studenti": studenti, "ucitelia": ucitelia, "odbory": odbory, "dostupnosti": dostupnosti, "message": "Prosím vyplňte všetky polia!"})
+        
+        if student == "-":
+            dostupnost = Dostupnost.objects.get(nazov='voľné')
+            pocet_kontrol = 0
+            student = None
+            tema = Tema(
+                nazov = request.POST['nazov'],
+                popis = request.POST['popis'],
+                konzultant = Ucitel.objects.get(id=konzultant),
+                student = None,
+                odbor = Odbor.objects.get(id=odbor),
+                dostupnost = Dostupnost.objects.get(id=dostupnost.id),
+                pocet_kontrol = request.POST['pocet_kontrol']
+            )
+        else:
+            existing_tema = Tema.objects.filter(student=student).exists()
+            if existing_tema:
+                return render(request, 'soc/add_tema.html', {"studenti": studenti, "ucitelia": ucitelia, "odbory": odbory, "dostupnosti": dostupnosti, "message": "Študent už má zadanú tému!"})
+            else:
+                tema = Tema(
+                    nazov = request.POST['nazov'],
+                    popis = request.POST['popis'],
+                    konzultant = Ucitel.objects.get(id=konzultant),
+                    student = Student.objects.get(id=student),
+                    odbor = Odbor.objects.get(id=odbor),
+                    dostupnost = Dostupnost.objects.get(id=dostupnost),
+                    pocet_kontrol = request.POST['pocet_kontrol']
+                )
+        
+        tema.save()
+        return render(request, 'soc/add_tema.html', {"studenti": studenti, "ucitelia": ucitelia, "odbory": odbory, "dostupnosti": dostupnosti, "message": "Téma bola úspešne pridaná!"})
